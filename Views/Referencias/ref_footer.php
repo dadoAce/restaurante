7855d91a466e2940e19a15fe9c5373eff4af0c55
dado_ <dado_@DESKTOP-81M3N9K>
2021-04-23 18:45:51 -0500

    Se eliminaron archivos, solo se dejo lo necesario para el Ejemplo

--- Views/Referencias/ref_footer.php
+++ Views/Referencias/ref_footer.php
@@ -1,5 +1,1 @@
  
-<script src="<?= $this->base_url();?>/Assets/local/jquery-3.5.1.slim.min.js" ></script>
-<script src="<?= $this->base_url();?>/Assets/local/jquery.min.js"></script>
-<script src="<?= $this->base_url();?>/Assets/local/popper.min.js"></script>
-<script src="<?= $this->base_url();?>/Assets/local/bootstrap.min.js" "></script> 
--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
- 
-require_once 'libs/App.php';
-require_once 'libs/Modelo.php'; 
-$app = new App();
